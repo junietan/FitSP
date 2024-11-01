175741a30550e08e2776525cfdc6406fc673c8d8
junietan <junietan@sp.edu.sg>
2024-11-01 10:14:23 +0800

    Update challengeModel.js

--- src/models/challengeModel.js
+++ src/models/challengeModel.js
@@ -1,14 +1,1 @@
 const pool = require('../services/db');
-module.exports.viewCompletions = (challenge_id, callback) => {
-};
-module.exports.getChallengePoints = (challenge_id, callback) => {
-};
-
-module.exports.completeChallenge = (data, callback) => {
-};
-
-module.exports.updateUserPoints = (data, callback) => {
-};
-
-module.exports.view = (callback) => {
-}
