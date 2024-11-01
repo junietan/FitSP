a708e6ac2156de114247ede3930197c622b18d24
junietan <junietan@sp.edu.sg>
2024-11-01 10:13:50 +0800

    Update challengeController.js

--- src/controllers/challengeController.js
+++ src/controllers/challengeController.js
@@ -1,13 +1,2 @@
 const model = require("../models/challengeModel.js")
 
-module.exports.getChallengeCompletions = (req, res) => {
-};
-
-module.exports.computePoints = (req, res, next) => {
-}
-
-module.exports.completeChallenge = (req, res) => {
-};
-
-module.exports.getAllChallenges = (req, res) => {
-}
