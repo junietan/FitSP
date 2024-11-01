e859fedd7a55f7b45c3b61820dc8cfee85b3d498
junietan <junietan@sp.edu.sg>
2024-11-01 10:14:52 +0800

    Update challengeRoutes.js

--- src/routes/challengeRoutes.js
+++ src/routes/challengeRoutes.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
 const controller = require('../controllers/challengeController');
-
-router.post('/:challenge_id', controller.computePoints, controller.completeChallenge);
 
 
 module.exports = router;
