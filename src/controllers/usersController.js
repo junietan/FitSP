0400b8054b78ce52731a0357cba3fbc5e0166e87
junietan <junietan@sp.edu.sg>
2024-11-01 10:14:04 +0800

    Update usersController.js

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,10 +1,2 @@
 const model = require("../models/userModel.js")
 
-module.exports.updateUser = (req, res) => {
-};
-
-module.exports.addUser = (req, res) => {
-}
-
-module.exports.getAllUsers = (req, res) => {
-}
