8e4466d0209e415814e629a5afc623ecdd0ecdcb
junietan <junietan@sp.edu.sg>
2024-11-01 10:14:35 +0800

    Update userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,10 +1,1 @@
 const pool = require('../services/db');
-
-module.exports.update = (data, callback) => {
-};
-
-module.exports.create = (username, callback) => {
-};
-
-module.exports.view = (callback) => {
-}
